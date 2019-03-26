99b7932499d6ff1779fe5497d2414c2f580e43f2
Alexandr Ksenzov <alexander.ksenzov@gmail.com>
2019-03-26 19:11:15 +0200

    Attempt to save image profile

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(null=True, blank=True,)
-    avatar = models.ImageField('profile picture', upload_to='static/images/avatars/', null=True, blank=True)
+    avatar = models.ImageField('profile picture', upload_to='avatars', null=True, blank=True)
